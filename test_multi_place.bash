1e5e5407c7292a46730f6e710097603cd75194dd
Arjo Chakravarty <arjoc@google.com>
2024-08-27 20:47:57 +0800

    Fix Multistop test case
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- test_multi_place.bash
+++ test_multi_place.bash
@@ -7,7 +7,7 @@ if [ $ret -ne 0 ]; then
         exit -1
 fi
 
-ros2 run rmf_demos_tasks dispatch_go_to_place -p lounge -p pantry -F tinyRobot -R tinyRobot1 --use_sim_time
+ros2 run rmf_demos_tasks dispatch_go_to_place -p lounge pantry -F tinyRobot -R tinyRobot1 --use_sim_time
 ros2 run rmf_demos_tasks wait_for_task_complete -F tinyRobot -R tinyRobot1 --timeout 100
 
 ret=$?
